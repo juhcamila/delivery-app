374f301cee7eeea29325820ec4f7ada33a73e9a7
viniciussilveiragomes <dean.viniciussilveira@gmail.com>
2020-06-02 23:37:20 -0300

    Models Empresa/ItensComprados

---
+++ Models/Empresa.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace DeliveryApp.Empresa
+{
+    
+        
+    public Guid Id { get; set; }
+    [Required(ErrorMessage = "Campo obrigatório.")]
+    public String Nome { get; set; }
+    [Required(ErrorMessage = "Campo obrigatório.")]
+    [MinLength(12, ErrorMessage = "O campo CNPJ deve conter no mínimo 12 caracteres.")]
+    public String Cnpj { get; set; }
+    public String Telefone { get; set; }
+}
